21f38a868c3ea7d161b3f2c29842076511c29e13
sinlakshya17011 <sinlakshya17@gmail.com>
2026-09-02 20:50:16 +0530

    initial

--- E-Comm/src/App.jsx
+++ E-Comm/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Collection from './Pages/Collection'
+import About from './Pages/About'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-{5vw} md:pc-{7vw} lg:px-{9vw}'>
+
+      <Routes >
+        <Route path='/' element={Home} />
+        <Route path='/collection' element={Collection} />
+        <Route path='/about' element ={About} />
+       </Routes>
+    </div>
   )
 }
 
