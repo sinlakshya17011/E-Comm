60f94f24675efb3df545e24adcd2d3b677951480
sinlakshya17011 <sinlakshya17@gmail.com>
2026-09-08 10:49:37 +0530

    init

--- E-Comm/src/App.jsx
+++ E-Comm/src/App.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Collection from './Pages/Collection'
 import About from './Pages/About'
-
+import Contact from './Pages/Contact'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+import Login from './Pages/Login'
+import Placeorder from './Pages/Placeorder'
+import Orders from './Pages/Orders'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-{5vw} md:pc-{7vw} lg:px-{9vw}'>
-
-      <Routes >
-        <Route path='/' element={Home} />
-        <Route path='/collection' element={Collection} />
-        <Route path='/about' element ={About} />
-       </Routes>
+    <div  className='px-4 sm:px-{5vw} md:pc-{7vw} lg:px-{9vw}'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />}/>
+        <Route path='/Contact' element={<Contact />}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/place-order' element={<Placeorder />}/>
+        <Route path='/orders' element={<Orders />}/>
+      </Routes>
+      
     </div>
   )
 }
