232ef2fd97c9312fb1602d0f01be8c70d68b2409
sinlakshya17011 <sinlakshya17@gmail.com>
2026-09-17 14:11:18 +0530

    web

--- E-Comm/src/Components/Hero.jsx
+++ E-Comm/src/Components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
             <p className='w-8 md:w-11 h-0.5 bg-[#414141]'></p>
             <p className='font-medium text-sm md:text-base'>OUR BESTSELLER</p>
           </div>
-          <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Collection</h1>
+          <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
           <div className='flex items-center gap-2'>
             <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
             <p className='w-8 md:w-11 h-px bg-[#414141]'></p>
--- E-Comm/src/Components/LatestCollection.jsx
+++ E-Comm/src/Components/LatestCollection.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
+import Title from './Title'
 
 const LatestCollection = () => {
 
     const {products} = useContext(ShopContext)
-    console.log(products)
 
   return (
-    <div>
-     
+    <div className='my-10'>
+       <div className='text-3xl text-center py-10'>
+          <Title text1={'LATEST'} text2={'COLLECTION'} />
+       </div>
     </div>
   )
 }
